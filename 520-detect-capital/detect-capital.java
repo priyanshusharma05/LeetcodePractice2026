280d16183cf2f50683a06d6e8bb45bf284880644
Priyanshu sharma <142593082+priyanshusharma05@users.noreply.github.com>
2026-02-15 23:53:58 +0530

    Time: 1 ms (88.20%) | Memory: 43.5 MB (12.33%) - LeetSync

---
+++ 520-detect-capital/detect-capital.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean detectCapitalUse(String word) {
+        if(word.equals(word.toUpperCase()))return true;
+        if(word.equals(word.toLowerCase()))return true;
+        
+        for(int i=1;i<word.length();i++){
+            if(Character.isUpperCase(word.charAt(i)))return false;
+        }
+        return true;
+    }
+}
