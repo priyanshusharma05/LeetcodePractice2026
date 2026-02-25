b9319f9171066e1050762cd5652c514eee3a6143
Priyanshu sharma <142593082+priyanshusharma05@users.noreply.github.com>
2026-02-25 21:57:06 +0530

    Time: 16 ms (16.96%) | Memory: 47.3 MB (8.64%) - LeetSync

--- 1458-sort-integers-by-the-number-of-1-bits/sort-integers-by-the-number-of-1-bits.java
+++ 1458-sort-integers-by-the-number-of-1-bits/sort-integers-by-the-number-of-1-bits.java
@@ -7,12 +7,11 @@ class Solution {
             ls.add(Arrays.asList(arr[i],Integer.bitCount(arr[i])));
         }
         Collections.sort(ls, (a,b) -> Integer.compare(a.get(1),b.get(1)));
-        int[] re=new int[arr.length];
         int ind=0;
         for(List v:ls){
-            re[ind]=(int) v.get(0);
+            arr[ind]=(int) v.get(0);
             ind++;
         }
-        return re;
+        return arr;
     }
 }
