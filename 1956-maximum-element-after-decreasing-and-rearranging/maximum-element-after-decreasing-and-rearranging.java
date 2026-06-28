a23175659033352bbd81b20193f97709b383eddc
Priyanshu sharma <142593082+priyanshusharma05@users.noreply.github.com>
2026-06-29 01:34:44 +0530

    Time: 8 ms (83.33%) | Memory: 77.6 MB (41.46%) - LeetSync

--- 1956-maximum-element-after-decreasing-and-rearranging/maximum-element-after-decreasing-and-rearranging.java
+++ 1956-maximum-element-after-decreasing-and-rearranging/maximum-element-after-decreasing-and-rearranging.java
@@ -3,7 +3,7 @@ class Solution {
        Arrays.sort(arr);
        if(arr[0]!=1)arr[0]=1;
        for(int i=1;i<arr.length;i++){
-        if(Math.abs(arr[i]-arr[i-1])>1)arr[i]=arr[i-1]+1;
+        if((arr[i]-arr[i-1])>1)arr[i]=arr[i-1]+1;
        }
        return arr[arr.length-1];
     }
