dfb2cdb331b92a4bcc5b093c0173648031a861de
Priyanshu sharma <142593082+priyanshusharma05@users.noreply.github.com>
2026-07-01 22:59:42 +0530

    Time: 8 ms (91.30%) | Memory: 49 MB (73.59%) - LeetSync

--- 56-merge-intervals/merge-intervals.java
+++ 56-merge-intervals/merge-intervals.java
@@ -1,23 +1,23 @@
 class Solution {
-    public int[][] merge(int[][] arr) {
-        Arrays.sort(arr,(a,b)-> Integer.compare(a[0],b[0]));
+    public int[][] merge(int[][] intrv) {
+        Arrays.sort(intrv,(a,b)-> Integer.compare(a[0],b[0]));
         List<int[]> ls=new ArrayList<>();
-        int st=arr[0][0];
-        int end=arr[0][1];
-        for(int i=1;i<arr.length;i++){
-            if(arr[i][0]<=end){
-                end=Math.max(end,arr[i][1]);
+        int st=intrv[0][0];
+        int end=intrv[0][1];
+        for(int i=1;i<intrv.length;i++){
+            if(intrv[i][0]<=end){
+                end=Math.max(end,intrv[i][1]);
             }else{
                 ls.add(new int[] {st,end});
-                st=arr[i][0];
-                end=arr[i][1];
+                st=intrv[i][0];
+                end=intrv[i][1];
             }
         }
         ls.add(new int[] {st,end});
-        int[][] ans=new int[ls.size()][2];
+        int[][] re=new int[ls.size()][2];
         for(int i=0;i<ls.size();i++){
-            ans[i]=ls.get(i);
+            re[i]=ls.get(i);
         }
-        return ans;
+        return re;
     }
 }
