1e26b434ff7e361a9dd9d101ef970ce6155f42e3
Priyanshu sharma <142593082+priyanshusharma05@users.noreply.github.com>
2026-07-14 14:23:08 +0530

    Time: 0 ms (100.00%) | Memory: 53.2 MB (8.80%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.java
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.java
@@ -1,33 +1,36 @@
 class Solution {
     public int singleNonDuplicate(int[] nums) {
-
-        int low = 0;
-        int high = nums.length - 1;
-        int ans = -1;
-
-        while (low <= high) {
-            int mid = (low + high) / 2;
-            if (mid == 0 || mid == nums.length - 1) {
+        int st=0;
+        int end=nums.length-1;
+        if(nums.length==1)return nums[0];
+        while(st<=end){
+            int mid=(st+end)/2;
+            if(mid!=0 && mid!=nums.length-1 && (nums[mid]!=nums[mid+1] && nums[mid]!=nums[mid-1])){
                 return nums[mid];
+            }else if(mid==0){
+                if(nums[mid]!=nums[mid+1])return nums[mid];
+            }else if(mid==nums.length-1){
+                if(nums[mid]!=nums[mid-1])return nums[mid];
             }
-            if (nums[mid] != nums[mid + 1] && nums[mid] != nums[mid - 1]) {
-                ans = nums[mid];
-                break;
-            }
-            if (mid % 2 == 0) {
-                if (nums[mid] == nums[mid + 1]) {
-                    low = mid + 2;
-                } else {
-                    high = mid - 1;
+            if(iseven(mid)){
+                if(nums[mid]==nums[mid+1]){
+                    st=mid+1;
+                }else{
+                    end=mid-1;
                 }
-            } else {
-                if (nums[mid] == nums[mid - 1]) {
-                    low = mid + 1;
-                } else {
-                    high = mid - 1;
+            }else{
+                if(nums[mid]==nums[mid+1]){
+                    end=mid-1;
+                }else{
+                    st=mid+1;
                 }
             }
         }
-        return ans;
+        return -1;
+        
+    }
+    private boolean iseven(int n){
+        if(n%2==0)return true;
+        return false;
     }
 }
